f17d77d7801daff213e62dcf88457989b3affbe3
DenisdS <denis_santos1989@hotmail.com>
2020-03-23 16:19:26 -0300

    Simple config of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,14 @@
-module.exports = {
-  stories: ['../src/**/*.stories.js'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-  ],
-};
+import { configure } from '@storybook/react'
+import { addDecorator } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import requireContext from 'require-context.macro'
+
+addDecorator(withInfo);
+
+const req = requireContext('../src/', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
