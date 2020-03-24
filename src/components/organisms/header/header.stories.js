d8297e58f2c5c0411b9eeb181d73a1e03006b000
DenisdS <denis_santos1989@hotmail.com>
2020-03-24 20:46:39 -0300

    Delete Accessible menu

--- src/components/organisms/header/header.stories.js
+++ src/components/organisms/header/header.stories.js
@@ -3,17 +3,10 @@ import React from 'react';
 import { storiesOf } from "@storybook/react";
 
 import Header from './header';
-import AccessibleMenu from '../../molecules/accessibleMenu/menu'
 
 storiesOf("Header", module)
   .add("Header simple", () => (
     <Header>
       <img src="" alt="" />
-    </Header>
-  ))
-  .add("Header accessible", () => (
-    <Header>
-      <img src="" alt="" />
-      <AccessibleMenu />
     </Header>
   ));
