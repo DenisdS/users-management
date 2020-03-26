5f80cff7bb498452a616eb7d67e69970177f57eb
DenisdS <denis_santos1989@hotmail.com>
2020-03-26 14:10:49 -0300

    Add normalize CSS

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,10 +1,13 @@
 import React from 'react'
+
+import Normalize from 'react-normalize'
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App'
 
 const Root = () => (
   <Router>
+    <Normalize />
     <App />
   </Router>
 )
